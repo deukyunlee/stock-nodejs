7e78c704e03a57e14014a86e5bc3937b975f2607
deukyunlee <dukedev1004@gmail.com>
2022-02-04 01:08:31 +0900

    FIX: automatic insert into database

--- routes/stock3.js
+++ routes/stock3.js
@@ -6,12 +6,13 @@ const router = require("express").Router();
 // import got from "got";
 // router = router.Router();
 const request = require("request");
-
+var db = require("../app.js");
 const cheerio = require("cheerio");
 const fs = require("fs");
 const axios = require("axios");
 const { json } = require("express");
 const API_KEY = "JDBVQUW2HL07WAGK";
+let symbol;
 // import got from "got";
 
 // const got = require("got");
@@ -75,9 +76,10 @@ router.post("/", function (req, res) {
       //console.log(data);
       url = new Array();
       //   console.log(data[key].title);
+      symbol = data[key].title;
       url[
         key
-      ] = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${data[key].title}&outputsize=compact&apikey=${API_KEY}`;
+      ] = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=compact&apikey=${API_KEY}`;
       //console.log(url[key]);
 
       //   console.log(url[key]);
@@ -86,7 +88,34 @@ router.post("/", function (req, res) {
           method: "get",
           url: url[key],
         }).then((res) => {
-          console.log(res);
+          //   console.log(res.data);
+          var res2 = res.data;
+          var content = res2["Time Series (Daily)"];
+          console.log(content);
+
+          if (content) {
+            // get key for date
+            const keys = Object.keys(content);
+            // console.log(keys)
+
+            const sql = `insert IGNORE into daily(name, timestamp, open, high,low,close,volume) values (?)`;
+            console.log("1");
+
+            // extract and insert data from API into mysql DB
+            keys.forEach(function (key, index) {
+              const row = content[key];
+              const date = keys[index];
+              const open = parseFloat(row["1. open"]);
+              const high = parseFloat(row["2. high"]);
+              const low = parseFloat(row["3. low"]);
+              const close = parseFloat(row["4. close"]);
+              const volume = parseInt(row["5. volume"]);
+              const array = [symbol, date, open, high, low, close, volume];
+              db.query(sql, [array], function (err, rows, fields) {
+                // console.log(symbol)
+              });
+            });
+          }
           //   res2 = JSON.parse(res);
           //   res2 = JSON.parse(res);
           //   res2 = JSON.stringify(res);
