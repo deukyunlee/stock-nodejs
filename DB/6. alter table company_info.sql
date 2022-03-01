890ba1044a9fd96bb78494d921d71ffe88afc5bc
deukyunlee <dukedev1004@gmail.com>
2022-03-01 21:27:18 +0900

    FEAT: Sequence insert

--- DB/6. alter table company_info.sql
+++ DB/6. alter table company_info.sql
@@ -2,3 +2,4 @@ alter table company_info add column id INT;
 alter table company_info drop primary key;
 alter table company_info add primary key (id, symbol);
 ALTER TABLE company_info MODIFY ID INT AUTO_INCREMENT;
+ALTER TABLE company_info add column updatedAt DATE;
--- routes/post/postDailyData.js
+++ routes/post/postDailyData.js
@@ -8,234 +8,63 @@ const API_KEY = process.env.ALPHAVANTAGEAPI;
 
 //sql = `select LEAD(symbol, ${id}) over (order by symbol) from daily group by symbol;`;
 
+//select symbol from daily group by symbol having max(timestamp) = '2022-02-25';
 router.post("/", function (req, res, next) {
-  async function SymbolExists(curId) {
-    let count = 500;
-    let url = [];
-    let resApi;
-    let resData;
-    let content;
-    let symbol;
-    // console.log(curId[0].id);
-    let id = curId[0].id;
-    count -= id;
-    let sql = `select LEAD(symbol, ${id}) over (order by symbol) as list from company_info group by symbol;`;
+  let symbol = "A";
+  async function getDaily() {
+    let url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=compact&apikey=${API_KEY}`;
     try {
-      console.log("here");
-      db.query(sql, async (err, result) => {
-        if (result.length !== 0) {
-          for (var i in result) {
-            await delayFunc.sleep(12050);
-            id += 1;
-            count -= 1;
-            symbol = result[i].list;
-            if (symbol) {
-              try {
-                url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=compact&apikey=${API_KEY}`;
-                // console.log(url);
-              } catch {
-                console.log("symbol or url not found");
-              }
-            }
-            try {
-              resApi = await axios({
-                method: "get",
-                url: url,
-              });
-              // console.log(resApi);
-            } catch {
-              console.log("axios failed");
-            }
-
-            try {
-              resData = resApi.data;
-            } catch {
-              console.log("no data");
-            }
-
-            try {
-              content = await resData["Time Series (Daily)"];
-              if (content) {
-                const keys = Object.keys(content);
-
-                let sql = `insert IGNORE into daily(symbol, timestamp, open, high,low,close,volume) values (?)`;
-
-                console.log(
-                  symbol +
-                    " inserted into database : " +
-                    count +
-                    " symbols left"
-                );
-
-                keys.forEach(function (key, index) {
-                  const row = content[key];
-                  const date = keys[index];
-                  const open = parseFloat(row["1. open"]);
-                  const high = parseFloat(row["2. high"]);
-                  const low = parseFloat(row["3. low"]);
-                  const close = parseFloat(row["4. close"]);
-                  const volume = parseInt(row["5. volume"]);
-                  const array = [symbol, date, open, high, low, close, volume];
-                  db.query(sql, [array], function (err, rows, fields) {});
-                });
-
-                let sql2 = `UPDATE sequence SET id = ${id}, symbol = '${symbol}' where t_name = "daily";`;
-                db.query(sql2, function (err, rows, fields) {});
-
-                // console.log(id);
-              }
-            } catch {
-              console.log("sql error");
-            }
-            // console.log(array);
-          }
-        } else {
-          DbQuery();
-        }
+      resApi = await axios({
+        method: "get",
+        url: url,
       });
+      console.log(resApi);
+    } catch {
+      console.log("axios failed");
+    }
+
+    try {
+      resData = resApi.data;
     } catch {
       console.log("no data");
     }
-  }
 
-  let sql = `select id from sequence where t_name = "daily";`;
-
-  db.query(sql, (err, countId) => {
     try {
-      if (countId.length !== 0) SymbolExists(countId);
-      else DbQuery();
-    } catch (exception) {
-      console.log(exception);
-    }
-  });
+      content = await resData["Time Series (Daily)"];
+      if (content) {
+        const keys = Object.keys(content);
 
-  //const sql = `select symbol from (select symbol, rank() over (order by cap desc) as 'ranking' from company_info) ranked where ranked.ranking <=100;`;
+        let sql = `insert IGNORE into daily(symbol, timestamp, open, high,low,close,volume) values (?)`;
 
-  // 500개의 데이터 중 시총 순으로 100개만 정렬해서 DB에 들어감
-  // select symbol from daily;
-  // .then 블럭 async/await으로 리팩토링 필요
-  async function SymbolNotExists(countResult) {
-    let symbol;
-    let count = 500;
-    let url = [];
-    let res;
-    let resData;
-    let content;
-    let id = 0;
-    for (var i in countResult) {
-      await delayFunc.sleep(12050);
-      id += 1;
-      try {
-        symbol = countResult[i].symbol;
+        // console.log(
+        //   symbol + " inserted into database : " + count + " symbols left"
+        // );
 
-        for (var i in symbol) {
-          url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=compact&apikey=${API_KEY}`;
-        }
-      } catch {
-        console.log("symbol or url not found");
-      }
-      try {
-        res = await axios({
-          method: "get",
-          url: url,
+        keys.forEach(function (key, index) {
+          const row = content[key];
+          const date = keys[index];
+          const open = parseFloat(row["1. open"]);
+          const high = parseFloat(row["2. high"]);
+          const low = parseFloat(row["3. low"]);
+          const close = parseFloat(row["4. close"]);
+          const volume = parseInt(row["5. volume"]);
+          const array = [symbol, date, open, high, low, close, volume];
+          db.query(sql, [array], function (err, rows, fields) {});
         });
-      } catch {
-        console.log("axios failed");
+        let sql = `select max(timestamp) from daily where symbol = 'a'`;
+        db.query(sql, function (err, rows, fields) {
+          sql = `INSERT INTO company_info(updatedAt) VALUES(?) ON DUPLICATE KEY UPDATE updatedAt = ?`;
+
+          db.query(sql);
+        });
+
+        // console.log(id);
       }
-
-      try {
-        resData = res.data;
-      } catch {
-        console.log("no data");
-      }
-
-      try {
-        content = await resData["Time Series (Daily)"];
-        if (content) {
-          const keys = Object.keys(content);
-
-          const sql = `insert IGNORE into daily(symbol, timestamp, open, high,low,close,volume) values (?)`;
-
-          console.log(
-            symbol + " inserted into database : " + count - id + " symbols left"
-          );
-
-          keys.forEach(function (key, index) {
-            const row = content[key];
-            const date = keys[index];
-            const open = parseFloat(row["1. open"]);
-            const high = parseFloat(row["2. high"]);
-            const low = parseFloat(row["3. low"]);
-            const close = parseFloat(row["4. close"]);
-            const volume = parseInt(row["5. volume"]);
-            const array = [symbol, date, open, high, low, close, volume];
-            db.query(sql, [array], function (err, rows, fields) {});
-          });
-          let sql2 = `UPDATE sequence SET id = ${id}, symbol = '${symbol}' where t_name = "daily";`;
-          db.query(sql2, function (err, rows, fields) {});
-
-          console.log(id);
-        }
-      } catch {
-        console.log("sql error");
-      }
-      // console.log(array);
+    } catch {
+      console.log("sql error");
     }
   }
-  function DbQuery() {
-    let sql = `select symbol from company_info`;
-    db.query(sql, (err, countResult) => {
-      try {
-        let sql3 = `INSERT INTO sequence VALUES(1, "daily", "A")`;
-        db.query(sql3, function (err, rows, fields) {});
-        SymbolNotExists(countResult);
-      } catch (exception) {
-        console.log(exception);
-      }
-    });
-  }
-});
-
-router.get("/full-data/:symbol", function (req, res) {
-  const symbol = req.params.symbol;
-  const sql = `SELECT * from daily where symbol = ?`;
-  db.query(sql, symbol, function (err, rows, fields) {
-    // res.json(rows);
-    try {
-      res.json(rows);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-});
-
-router.get("/interval", (req, res) => {
-  const symbol = req.query.symbol;
-  const period = req.query.period;
-  const startDate = req.query.startDate;
-  const endDate = req.query.endDate;
-  let interval = "";
-  let date_notYear = "";
-  let sql = "";
-
-  if (req.query.interval > 0) interval = "/" + req.query.interval;
-
-  if (period != "year") date_notYear = 'DATE_FORMAT(timestamp, "%y"), ';
-
-  /* high: highest, low: lowest volume: sum other than that is as it is*/
-  sql = `SELECT symbol, DATE_FORMAT(timestamp,'%Y-%m-%d') as date, open,
-         max(high) as high, min(low) as low, close, sum(volume)
-         FROM daily WHERE symbol = '${symbol}' AND timestamp BETWEEN '${startDate}' AND '${endDate}' GROUP BY ${date_notYear}
-         FLOOR(${period}(timestamp)${interval}) ORDER BY date;`;
-
-  /*select name, timestamp, open, max(high) as high, min(low) as low, close, sum(volume) as volume from daily where name = 'aapl' and timestamp between '2020-01-01' and '2022-01-01' group by date_format(timestamp, "%y"), FLOOR(month(timestamp)/1) order by timestamp;*/
-  db.query(sql, function (err, rows, fields) {
-    try {
-      res.json(rows);
-    } catch (err) {
-      console.log(err);
-    }
-  });
+  getDaily();
 });
 
 module.exports = router;
