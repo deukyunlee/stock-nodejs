4b7a08e62904e76080cb74f7eb2085580da92e25
deukyunlee <dukedev1004@gmail.com>
2022-03-04 18:02:20 +0900

    FIX: change_percent, change_value NULL err

--- DB/1. create table(daily, intraday, company_info).sql
+++ DB/1. create table(daily, intraday, company_info).sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS daily (symbol VARCHAR(50), date DATE NOT NULL, open DECIMAL(10,4) NOT NULL, high DECIMAL(10,4) NOT NULL, low DECIMAL(10,4) NOT NULL, close DECIMAL(10,4) NOT NULL, volume INT NOT NULL, change_percent DECIMAL(5,4), change_value DECIMAL(5,4) constraint daily_PK primary key(symbol, date));
+CREATE TABLE IF NOT EXISTS daily (symbol VARCHAR(50), date DATE NOT NULL, open DECIMAL(10,4) NOT NULL, high DECIMAL(10,4) NOT NULL, low DECIMAL(10,4) NOT NULL, close DECIMAL(10,4) NOT NULL, volume INT NOT NULL, change_percent DECIMAL(10,4), change_value DECIMAL(10,4) constraint daily_PK primary key(symbol, date));
 
-CREATE TABLE IF NOT EXISTS intraday (symbol VARCHAR(50), datetime DATETIME NOT NULL, open DECIMAL(10,4) NOT NULL, high DECIMAL(10,4) NOT NULL, low DECIMAL(10,4) NOT NULL, close DECIMAL(10,4) NOT NULL, volume INT NOT NULL, change_percent DECIMAL(5,4), change_value DECIMAL(5,4) constraint daily_PK primary key(symbol, datetime));
+CREATE TABLE IF NOT EXISTS intraday (symbol VARCHAR(50), datetime DATETIME NOT NULL, open DECIMAL(10,4) NOT NULL, high DECIMAL(10,4) NOT NULL, low DECIMAL(10,4) NOT NULL, close DECIMAL(10,4) NOT NULL, volume INT NOT NULL, change_percent DECIMAL(10,4), change_value DECIMAL(10,4) constraint daily_PK primary key(symbol, datetime));
 
 CREATE TABLE IF NOT EXISTS company_info(symbol VARCHAR(10), name_en VARCHAR(50), name_kr VARCHAR(50), desc_en TEXT, desc_kr TEXT, cap BIGINT, updatedAt_daily DATE, updatedAt_intraday DATETIME constraint company_info_PK primary key(symbol));
 
--- routes/post/postDailyData3.js
+++ routes/post/postDailyData3.js
@@ -4,8 +4,7 @@ const axios = require("axios");
 const delayFunc = require("../delayFuncs");
 const API_KEY = process.env.ALPHAVANTAGEAPI;
 router.post("/", function (req, res, next) {
-    
-  let sql = `select date,close, (close - lag(close, 1) over (order by date)) as value, ((close-lag(close, 1) over (order by date))/lag(close, 1) over (order by date)*100) as percent from daily where symbol = 'aapl';`;
+  let sql = `select date, (close - lag(close, 1) over (order by date)) as value, ((close - lag(close, 1) over (order by date))/ lag(close, 1) over (order by date)*100) as percent from daily where symbol = 'aapl';`;
   db.query(sql, function (err, rows, fields) {
     for (var i in rows) {
       let date = rows[i].date;
@@ -17,5 +16,6 @@ router.post("/", function (req, res, next) {
     }
   });
 });
+// | AAPL   | 2022-01-28 |  165.7100 |  170.3500 |  162.8000 |  170.3300 | 179935660 |           NULL |       6.9778 |
 
 module.exports = router;
