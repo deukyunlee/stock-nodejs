29f56228f9938d9b5ddce385550b0b77cabf655c
deukyunlee <dukedev1004@gmail.com>
2022-04-08 22:56:10 +0900

    Refactor: company

--- src/controllers/backup.js
+++ src/controllers/backup.js
@@ -5,42 +5,43 @@ const API_KEY = process.env.ALPHAVANTAGEAPI;
 module.exports.insert_company_cap = async function getSymbol() {
   let count = 500;
   let sql = `select symbol from company_info`;
-  url = [];
-  // var arr = new Array();
+
+  var arr = [];
   db.query(sql, (err, result) => {
-    Object.keys(result).forEach(async function (key) {
+    Object.keys(result).forEach(function (key) {
       // console.log(result[key].symbol);
 
-      // arr[key] = result[key].symbol;
-
-      // console.log(result[key].symbol);
+      url = new Array();
       symbol = result[key].symbol;
+
       url[
         key
       ] = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${API_KEY}`;
+
+      delayFunc.sleep(12050).then(() =>
+        axios({
+          method: "get",
+          url: url[key],
+        })
+          .then((res) => {
+            let res2 = res.data;
+            const marketCap = res2["MarketCapitalization"];
+            if (marketCap) {
+              const sql = `insert IGNORE into company_info(cap) values (?) where symbol =?`;
+              count -= 1;
+              console.log(
+                symbol + " inserted into database : " + count + " symbols left"
+              );
+              const array = [marketCap, symbol];
+              db.query(sql, [array], function (err, rows, fields) {});
+            }
+          })
+          .catch(() => {
+            console.log("rejected");
+          })
+      );
     });
   });
-  // console.log(url[key].toString());
-  for (var key in url) {
-    await delayFunc.sleep(12050);
-    resApi = await axios({
-      method: "get",
-      url: url[key],
-    });
-    let resData = resApi.data;
-
-    const marketCap = await resData["MarketCapitalization"];
-    if (marketCap) {
-      const sql = `insert IGNORE into company_info(cap) values (?) where symbol =?`;
-      count -= 1;
-      console.log(
-        symbol + " inserted into database : " + count + " symbols left"
-      );
-      const array = [marketCap, symbol];
-      db.query(sql, [array], function (err, rows, fields) {});
-    }
-  }
-  // console.log(arr[0]);
 
   // const data = await crawling.crawlSymbol();
   // for (var key in data) {
